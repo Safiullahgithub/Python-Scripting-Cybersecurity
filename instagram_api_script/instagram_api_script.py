c9ea4ff4c45590a857954a7106134cc67e529275
Safi Ullah Khan <141447767+Safiullahgithub@users.noreply.github.com>
2024-02-13 14:31:39 +0500

    Add files via upload

--- instagram_api_script/instagram_api_script.py
+++ instagram_api_script/instagram_api_script.py
@@ -1,1 +1,24 @@
+import requests
 
+# Instagram API credentials
+app_id = 'YOUR_APP_ID'
+app_secret = 'YOUR_APP_SECRET'
+redirect_uri = 'YOUR_REDIRECT_URI'
+access_token = 'YOUR_ACCESS_TOKEN'
+
+# User ID of the Instagram account
+user_id = 'YOUR_USER_ID'
+
+# Endpoint for user profile
+url = f'https://graph.instagram.com/{user_id}?fields=id,username&access_token={access_token}'
+
+# Send GET request to the API endpoint
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    user_data = response.json()
+    print(f"User ID: {user_data['id']}")
+    print(f"Username: {user_data['username']}")
+else:
+    print("Failed to retrieve user data")
